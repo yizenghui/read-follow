908810d589a8cafcddc47bace1228faaa08e73e3
yizenghui <zenghuitrue@gmail.com>
2017-07-10 08:31:54 +0800

    select

--- job/main.go
+++ job/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"time"
 
+	"github.com/hprose/hprose-golang/rpc"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
-	"github.com/yizenghui/read-follow/core/spider/qidian"
+	"github.com/yizenghui/read-follow/spider"
 )
 
 // Book 书籍模型
@@ -22,6 +24,7 @@ type Book struct {
 	ChapterURL string
 	AuthorURL  string
 	IsVIP      bool
+	PublishAt  int64 `sql:"index" default:"0"`
 }
 
 var db *gorm.DB
@@ -40,12 +43,41 @@ func main() {
 	db.AutoMigrate(&Book{})
 	defer db.Close()
 
+	go PostTask()
 	syncUpdateList()
-
 }
 
 func syncUpdateList() {
+	// go qidian()
+	// go zongheng()
+	// seventeenk()
+	ticker1 := time.NewTicker(time.Minute * 2)
+	for _ = range ticker1.C {
+		go spiderBookList("http://a.qidian.com/?orderId=5&page=1&style=2")
+		go spiderBookList("http://book.zongheng.com/store/c0/c0/b0/u0/p1/v9/s9/t0/ALL.html")
+		go spiderBookList("http://all.17k.com/lib/book/2_0_0_0_0_0_0_0_1.html")
+	}
+
+}
+
+func qidian() {
 	url := "http://a.qidian.com/?orderId=5&page=1&style=2"
+	ticker := time.NewTicker(time.Minute * 2)
+	for _ = range ticker.C {
+		fmt.Printf("ticked at %v spider %v \n", time.Now(), url)
+		go spiderBookList(url)
+	}
+}
+func zongheng() {
+	url := "http://book.zongheng.com/store/c0/c0/b0/u0/p1/v9/s9/t0/ALL.html"
+	ticker := time.NewTicker(time.Minute * 2)
+	for _ = range ticker.C {
+		fmt.Printf("ticked at %v spider %v \n", time.Now(), url)
+		go spiderBookList(url)
+	}
+}
+func seventeenk() {
+	url := "http://all.17k.com/lib/book/2_0_0_0_0_0_0_0_1.html"
 	ticker := time.NewTicker(time.Minute * 2)
 	for _ = range ticker.C {
 		fmt.Printf("ticked at %v spider %v \n", time.Now(), url)
@@ -54,7 +86,7 @@ func syncUpdateList() {
 }
 
 func spiderBookList(url string) {
-	rows, err := qidian.GetUpdateRows(url)
+	rows, err := spider.GetUpdate(url)
 	if err == nil {
 		for _, info := range rows {
 			time.Sleep(1 * time.Second)
@@ -64,7 +96,7 @@ func spiderBookList(url string) {
 }
 
 // 同步职位
-func syncBook(info qidian.UpdateItem) {
+func syncBook(info spider.Book) {
 
 	var book Book
 	db.Where(Book{BookURL: info.BookURL}).FirstOrCreate(&book)
@@ -83,4 +115,64 @@ func syncBook(info qidian.UpdateItem) {
 		fmt.Printf("%v  %v  %v\n", book.ID, book.Name, book.Chapter)
 	}
 
+}
+
+//PostTask 同步任务
+func PostTask() {
+	ticker := time.NewTicker(time.Second * 2)
+	for _ = range ticker.C {
+		go Publish()
+	}
+}
+
+//Stub rpc 服务器提供接口
+type Stub struct {
+	Save      func(string) (string, error)
+	AsyncSave func(func(string, error), string) `name:"save"`
+}
+
+// Publish 发布
+func Publish() {
+	var book Book
+	db.Where("publish_at = 0").First(&book)
+	if book.ID > 0 {
+		book.PublishAt = time.Now().Unix()
+		db.Save(&book)
+		client := rpc.NewClient("http://127.0.0.1:819/")
+		var stub *Stub
+		client.UseService(&stub)
+		postBook := TransformBook(book)
+		if jsonStr, err := json.Marshal(postBook); err == nil {
+			_, err := stub.Save(string(jsonStr))
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
+}
+
+// PostBook 提交转换的数据结构
+type PostBook struct {
+	Name       string `json:"name"`        // 地区
+	Chapter    string `json:"chapter"`     // 最小月薪
+	ChapterURL string `json:"chapter_url"` // 最大月薪
+	Author     string `json:"author"`      // 最大月薪
+	AuthorURL  string `json:"author_url"`  // 学历
+	BookURL    string `json:"book_url"`    // 工作经验
+	Total      string `json:"total"`       // string默认长度为255, 使用这种tag重设。
+	IsVIP      bool   `json:"is_vip"`      // string默认长度为255, 使用这种tag重设。
+}
+
+// TransformBook 数据转换
+func TransformBook(book Book) PostBook {
+	var pb PostBook
+	pb.Name = book.Name
+	pb.Chapter = book.Chapter
+	pb.ChapterURL = book.ChapterURL
+	pb.Author = book.Author
+	pb.AuthorURL = book.AuthorURL
+	pb.BookURL = book.BookURL
+	pb.Total = book.Total
+	pb.IsVIP = book.IsVIP
+	return pb
 }
