b3fcbd52b738466ddab1bba4d459246100867aa3
yizenghui <zenghuitrue@gmail.com>
2017-07-19 23:45:07 +0800

    ss

--- core/notifications/follow.go
+++ core/notifications/follow.go
@@ -2,17 +2,31 @@ package notifications
 
 import (
 	"github.com/chanxuehong/wechat.v2/mp/core"
-	"github.com/chanxuehong/wechat.v2/mp/user"
+	"github.com/chanxuehong/wechat.v2/mp/message/template"
 )
 
 // Follow 关注通知
 /*
 	新追XXX
 */
-func Follow(OpenID string) (info *user.UserInfo, err error) {
+func Follow(ToUser, Name string) (msgID int64, err error) {
 
 	ats := core.NewDefaultAccessTokenServer(wxAppID, wxAppSecret, nil)
 	clt := core.NewClient(ats, nil)
-	// user,err := user.Get(clt,OpenID,"zh_CN")
-	return user.Get(clt, OpenID, "zh_CN")
+
+	// FollowMSG 关注通知消息结构
+	type FollowMSG struct {
+		Name template.DataItem `json:"name"`
+	}
+
+	msg := template.TemplateMessage2{
+		ToUser:     ToUser,
+		TemplateId: "jN1NL9EJvG2bD8EtejiH6liVcxynfHhcl-AlwEwM-l0",
+		URL:        "",
+		Data: FollowMSG{
+			Name: template.DataItem{Value: Name, Color: ""},
+		},
+	}
+
+	return template.Send(clt, msg)
 }
